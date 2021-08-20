c8478b443b22e9e1d98cbd1b650ce6d980c1c1db
aaron staes <aaron.staes@gmail.com>
2021-08-20 14:28:34 +0200

    added multi photo support

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 
-  has_one_attached :photo
+  has_many_attached :photos
 
   validates :name, presence: true, uniqueness: true
 end
