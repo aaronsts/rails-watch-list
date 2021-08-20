5dac35e1ef8c1bc47534b5ac47891b727314e80c
aaron staes <aaron.staes@gmail.com>
2021-08-20 10:34:01 +0200

    created database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # List: index, show, new, create
+  # Bookmark: new, create, delete
+
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
